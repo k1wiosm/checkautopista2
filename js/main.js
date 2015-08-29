fc03264b9c80ac62cd0847c8654cea75ca5a35c5
k1wiosm <k1wiosm@gmail.com>
2015-08-29 22:00:38 +0200

    fixing NaN in permalink

--- js/main.js
+++ js/main.js
@@ -45,10 +45,10 @@ function updatePermalink (relID, lat, lon, z) {
 	if ($('#wNoMaxspeed .chk')[0].checked) { options.view += 'M' };
 	if ($('#wNone .chk')[0].checked) { options.view += 'X'; };
 	if (options.view=='') { options.view='-' };
-	if (options.relID!==undefined) { url += '&id='+options.relID; };
-	if (options.lat!==undefined) { url += '&lat='+options.lat; };
-	if (options.lon!==undefined) { url += '&lon='+options.lon; };
-	if (options.z!==undefined) { url += '&z='+options.z; };
+	if (!options.relID) { url += '&id='+options.relID; };
+	if (!options.lat) { url += '&lat='+options.lat; };
+	if (!options.lon) { url += '&lon='+options.lon; };
+	if (!options.z) { url += '&z='+options.z; };
 	if (options.view!=='tdenxuaALMX') { url += '&view='+options.view; };
 	url=url.replace('&','?');
 	window.history.replaceState('', '', url);
