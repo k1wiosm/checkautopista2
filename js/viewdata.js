aa455201813d3acb47e88454b07944e1923c6ef7
k1wiosm <k1wiosm@gmail.com>
2015-09-16 19:29:58 +0200

    Fix zoom bug on mobile

--- js/viewdata.js
+++ js/viewdata.js
@@ -174,7 +174,11 @@ Freeway.prototype.addToSidebar = function () {
 }
 
 Freeway.prototype.zoom = function () {
-	map.fitBounds(L.latLngBounds(L.latLng(this.bounds.minlat,this.bounds.minlon), L.latLng(this.bounds.maxlat,this.bounds.maxlon)),{paddingTopLeft: [410,0]});
+	map.fitBounds(
+		L.latLngBounds(L.latLng(this.bounds.minlat,this.bounds.minlon), 
+		L.latLng(this.bounds.maxlat,this.bounds.maxlon)),
+		$(window).width() >= 768 ? {paddingTopLeft: [410,0]} : {}
+	);
 }
 
 Way.prototype.getLength = function () {
